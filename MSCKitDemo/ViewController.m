6bae86ba2b57006b4e5756017459bc8ed9460b63
MiaoShichang <miaoshichang@163.com>
2015-10-29 14:43:20 +0800

    初始化项目

---
+++ MSCKitDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MSCKitDemo
+//
+//  Created by MiaoShichang on 15/10/29.
+//  Copyright © 2015年 MiaoShichang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
